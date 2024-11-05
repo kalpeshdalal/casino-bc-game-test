2bd8c98efdd9694c9592035371a571db48d1e5a8
kalpesh_jain <kalpeshdalal@sdlccorp.com>
2024-11-05 17:37:46 +0530

    routes update

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -15,12 +15,12 @@ const Login = () => {
         <InputComp type='password' placeholder="password" value={password} onChange={(e)=>setPassword(e.target.value)} />
       </div>
       <div>
-        <Link to="/forgotPassword" className='text-xs font-extrabold text-iconClr  text-right'>Forgot your password?</Link>
+        <Link to="/forgot-password" className='text-xs font-extrabold text-iconClr  text-right'>Forgot your password?</Link>
       </div>
 
       <PrimaryButton buttonLabel="Sign In" btnClass="w-full mt-6 mb-6 h-10 text-xs"/>
 
-      <div className='text-xs font-extrabold'>New to BC.GAME?<Link to="/SignUp"  className='ml-2 text-primaryClr'>Create account</Link></div>
+      <div className='text-xs font-extrabold'>New to BC.GAME?<Link to="/sign-up"  className='ml-2 text-primaryClr'>Create account</Link></div>
     </div>
   )
 }
